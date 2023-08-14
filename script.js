39b953665256f2d8a2a4137dd65551bd29171b3a
patrickcorrea <patrickcorrea07@gmail.com>
2023-08-14 12:18:23 -0300

    Deploy do projeto

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function openModal() {
+    document.querySelector("#myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.querySelector("#myModal").style.display = "none";
+}
+
+
+
