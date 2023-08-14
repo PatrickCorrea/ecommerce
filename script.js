1faa1aae6d12bad87a96cfe7524b3859c4296533
patrickcorrea <patrickcorrea07@gmail.com>
2023-08-14 15:22:30 -0300

    Adicionando a função remover do checkout.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//abrindo modal
 function openModal() {
     document.querySelector("#myModal").style.display = "block";
 }
@@ -7,5 +7,24 @@ function closeModal() {
     document.querySelector("#myModal").style.display = "none";
 }
 
+// Adicione um ouvinte de eventos para cada botão "Remover"
+const removeButtons = document.querySelectorAll('.remove-product');
+removeButtons.forEach(button => {
+    button.addEventListener('click', removeItem);
+});
 
+// Função para remover um item do carrinho
+function removeItem(event) {
+    const listItem = event.target.closest('.product-checkout');
+    if (listItem) {
+        listItem.remove();
+    }   
+}
 
+//Finalizando compra
+function checkout() {
+    const checkout = document.querySelector(".checkout")
+    checkout.addEventListener('click', () => {
+        alert('Compra finalizada com sucesso!')
+    });
+}
